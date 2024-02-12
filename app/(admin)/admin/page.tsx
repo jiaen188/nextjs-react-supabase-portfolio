10c978faa0b7fe72954ad845b8f39aa9ca5539cc
gulifan <gulifan@dxy.cn>
2024-02-12 15:47:30 +0800

    feat: 数据面板

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,3 +1,65 @@
+"use client";
+
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { useEffect, useReducer } from "react";
+
+interface dataReducer {
+  loading?: boolean;
+  project?: string;
+  blog?: string;
+}
+
 export default function AdminPage() {
-  return <div>admin home page</div>;
+  const [data, setData] = useReducer(
+    (prev: dataReducer, next: dataReducer) => {
+      return { ...prev, ...next };
+    },
+    {
+      loading: true,
+      project: "",
+      blog: "",
+    }
+  );
+
+  const fetchDashboardDetails = async () => {
+    const response = await fetch("/api/app").then((res) => res.json());
+
+    setData({
+      loading: false,
+      ...response,
+    });
+  };
+
+  useEffect(() => {
+    fetchDashboardDetails();
+  }, []);
+
+  return (
+    <div className="flex gap-5">
+      <Card className="w-52 p-4">
+        <CardTitle>Total projects</CardTitle>
+        <CardContent className="text-center mt-6">
+          {data.loading ? (
+            <div className="animate-pulse">
+              <h2 className="h-2 bg-slate-200 rounded"></h2>
+            </div>
+          ) : (
+            <h2 className="text-4xl font-semibold">{data?.project}</h2>
+          )}
+        </CardContent>
+      </Card>
+      <Card className="w-52 p-4">
+        <CardTitle>Total blogs</CardTitle>
+        <CardContent className="text-center mt-6">
+          {data.loading ? (
+            <div className="animate-pulse">
+              <h2 className="h-2 bg-slate-200 rounded"></h2>
+            </div>
+          ) : (
+            <h2 className="text-4xl font-semibold">{data?.blog}</h2>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
